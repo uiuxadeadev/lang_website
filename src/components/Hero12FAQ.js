1e962998d6f2827a6160970b915841557fc95b91
Tai <taiu.engineer@gmail.com>
2023-09-16 20:03:34 +0900

    refactor FAQ secssion

--- src/components/Hero12FAQ.js
+++ src/components/Hero12FAQ.js
@@ -1,10 +1,5 @@
 import React from "react";
 import {
-  Link,
-  Box,
-  Button,
-  Flex,
-  Image,
   Stack,
   Text,
   Accordion,
@@ -12,9 +7,73 @@ import {
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
+  Box,
+  Button,
+  Flex,
 } from "@chakra-ui/react";
 
+const FAQItem = ({ question, answer }) => (
+  <AccordionItem
+    boxShadow="0px 2px 2px 0px rgba(0, 0, 0, 0.38)"
+    borderRadius="8px"
+    mb="16px"
+  >
+    <h2>
+      <AccordionButton h="64px" p="16px 24px">
+        <Box
+          as="span"
+          flex="1"
+          fontSize="24px"
+          fontWeight="600"
+          lineHeight="32px"
+          letterSpacing="0"
+          textAlign="left"
+          color="#666666"
+        >
+          {question}
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel
+      backgroundColor="#F2F4F7"
+      fontSize="18px"
+      fontWeight="400"
+      lineHeight="28px"
+      letterSpacing="0"
+      textAlign="left"
+      color="#000000"
+      alignItems="left"
+      p="32px 24px"
+    >
+      {answer}
+    </AccordionPanel>
+  </AccordionItem>
+);
+
 const Hero12FAQ = () => {
+  const faqData = [
+    {
+      question: "How can I join the session?",
+      answer:
+        "Please check our session list page to see all session times. They are all displayed in your local time zone.",
+    },
+    {
+      question: "How can I join the session?",
+      answer:
+        "Please check our session list page to see all session times. They are all displayed in your local time zone.",
+    },
+    {
+      question: "How can I join the session?",
+      answer:
+        "Please check our session list page to see all session times. They are all displayed in your local time zone.",
+    },
+    {
+      question: "Another FAQ question?",
+      answer: "Answer to another FAQ question.",
+    },
+  ];
+
   return (
     <>
       <Stack
@@ -38,157 +97,9 @@ const Hero12FAQ = () => {
         </Flex>
 
         <Accordion defaultIndex={[0]} allowMultiple w="600px" maxW="600px">
-          <AccordionItem
-            boxShadow="0px 2px 2px 0px rgba(0, 0, 0, 0.38)"
-            borderRadius="8px"
-            mb="16px"
-          >
-            <h2>
-              <AccordionButton h="64px" p="16px 24px">
-                <Box
-                  as="span"
-                  flex="1"
-                  fontSize="24px"
-                  fontWeight="600"
-                  lineHeight="32px"
-                  letterSpacing="0"
-                  textAlign="left"
-                  color="#666666"
-                >
-                  How can I join the session?
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel
-              backgroundColor="#F2F4F7"
-              fontSize="18px"
-              fontWeight="400"
-              lineHeight="28px"
-              letterSpacing="0"
-              textAlign="left"
-              color="#000000"
-              alignItems="left"
-              p="32px 24px"
-            >
-              Please check our session list page to see all session times. They
-              are all displayed in your local time zone
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem
-            boxShadow="0px 2px 2px 0px rgba(0, 0, 0, 0.38)"
-            borderRadius="8px"
-            mb="16px"
-          >
-            <h2>
-              <AccordionButton h="64px" p="16px 24px">
-                <Box
-                  as="span"
-                  flex="1"
-                  fontSize="24px"
-                  fontWeight="600"
-                  lineHeight="32px"
-                  letterSpacing="0"
-                  textAlign="left"
-                  color="#666666"
-                >
-                  How can I join the session?
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel
-              backgroundColor="#F2F4F7"
-              fontSize="18px"
-              fontWeight="400"
-              lineHeight="28px"
-              letterSpacing="0"
-              textAlign="left"
-              color="#000000"
-              alignItems="left"
-              p="32px 24px"
-            >
-              Please check our session list page to see all session times. They
-              are all displayed in your local time zone
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem
-            boxShadow="0px 2px 2px 0px rgba(0, 0, 0, 0.38)"
-            borderRadius="8px"
-            mb="16px"
-          >
-            <h2>
-              <AccordionButton h="64px" p="16px 24px">
-                <Box
-                  as="span"
-                  flex="1"
-                  fontSize="24px"
-                  fontWeight="600"
-                  lineHeight="32px"
-                  letterSpacing="0"
-                  textAlign="left"
-                  color="#666666"
-                >
-                  How can I join the session?
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel
-              backgroundColor="#F2F4F7"
-              fontSize="18px"
-              fontWeight="400"
-              lineHeight="28px"
-              letterSpacing="0"
-              textAlign="left"
-              color="#000000"
-              alignItems="left"
-              p="32px 24px"
-            >
-              Please check our session list page to see all session times. They
-              are all displayed in your local time zone
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem
-            boxShadow="0px 2px 2px 0px rgba(0, 0, 0, 0.38)"
-            borderRadius="8px"
-            mb="16px"
-          >
-            <h2>
-              <AccordionButton h="64px" p="16px 24px">
-                <Box
-                  as="span"
-                  flex="1"
-                  fontSize="24px"
-                  fontWeight="600"
-                  lineHeight="32px"
-                  letterSpacing="0"
-                  textAlign="left"
-                  color="#666666"
-                >
-                  How can I join the session?
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel
-              backgroundColor="#F2F4F7"
-              fontSize="18px"
-              fontWeight="400"
-              lineHeight="28px"
-              letterSpacing="0"
-              textAlign="left"
-              color="#000000"
-              alignItems="left"
-              p="32px 24px"
-            >
-              Please check our session list page to see all session times. They
-              are all displayed in your local time zone
-            </AccordionPanel>
-          </AccordionItem>
+          {faqData.map((faq, index) => (
+            <FAQItem key={index} {...faq} />
+          ))}
         </Accordion>
 
         <Flex alignItems="center" justifyContent="center" mt="100px">
@@ -198,12 +109,12 @@ const Hero12FAQ = () => {
             p="10px 20px"
             borderRadius="8px"
             boxShadow="0px 2px 2px 0px rgba(0, 0, 0, 0.38)"
-            bgGradient="linear(to-br, #00C2FF 3.94%, #8F00FF 94.16%)" // Gradient background
+            bgGradient="linear(to-br, #00C2FF 3.94%, #8F00FF 94.16%)"
             color="white"
-            transition="background 0.3s, box-shadow 0.3s" // Transition for background and box-shadow
+            transition="background 0.3s, box-shadow 0.3s"
             _hover={{
-              bgGradient: "linear(to-br, #00F0FF 0.77%, #0047FF 99.23%)", // Gradient background on hover
-              boxShadow: "0px 12px 12px 0px rgba(0, 0, 0, 0.38)", // Box shadow on hover
+              bgGradient: "linear(to-br, #00F0FF 0.77%, #0047FF 99.23%)",
+              boxShadow: "0px 12px 12px 0px rgba(0, 0, 0, 0.38)",
             }}
           >
             Start for Free!
